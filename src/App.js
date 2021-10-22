251188f9650332e58782079300496461e20f5ce6
Richard Miller Wimmer <Richard.MillerWimmer@insight.com>
2021-10-22 16:11:33 -0400

    header, styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import Header from "./components/Header";
+import './styles/app.scss';
 
 function App() {
   return (
     <div className="App">
-    
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../styles/header.scss';
+
+const Header = (props) => {
+    return (
+        <header>
+            <div className='wrap'>
+                <div className='logo'>
+                    <h3>Logo</h3>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
