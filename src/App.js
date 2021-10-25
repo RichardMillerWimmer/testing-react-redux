09d011ee267a158c9b8cf0a04196f34fef3291e9
Richard Miller Wimmer <Richard.MillerWimmer@insight.com>
2021-10-25 11:35:51 -0400

    Headline testing, component, elements, and null render

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Header from "./components/Header";
+import Headline from "./components/Headline";
 import './styles/app.scss';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <main className='main'>
+        <Headline header='Posts' desc='Click the button to render posts' />
+      </main>
     </div>
   );
 }
