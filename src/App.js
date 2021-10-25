44d04139b429fe16535fb055fe0f56b6a9d2e7a0
Richard Miller Wimmer <Richard.MillerWimmer@insight.com>
2021-10-25 13:14:32 -0400

    working on proptypes testing in Headline

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import Header from "./components/Header";
 import Headline from "./components/Headline";
 import './styles/app.scss';
 
+/* This const is not used within our app.
+   Although we are passing it to the Headline Component
+   it is only here as an exampleof testing PropTypes */
+   const tempArr = [{
+    fName: 'Joe',
+    lName: 'Bloggs',
+    email: 'joebloggs@gmail.com',
+    age: 24,
+    onlineStatus: true
+  }];
+  
+
 function App() {
   return (
     <div className="App">
       <Header />
       <main className='main'>
-        <Headline header='Posts' desc='Click the button to render posts' />
+        <Headline header='Posts' desc='Click the button to render posts' tempArr={tempArr} />
       </main>
     </div>
   );
